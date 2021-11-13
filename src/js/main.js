9f7bcb7714c012aed5bd2a30490bf4479f07cf77
Mikhailo <mikhailo.kandyba0@gmail.com>
2021-11-13 13:14:34 +0300

    desktop version

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+
+$(".btn-menu").on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".menu-navigation-wrap").toggleClass('show-menu');
+    $("body").toggleClass('overflow-hidden');
+});
+
+$('.information-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+});
+
+
+
+
+
+
+
